2e1b15af7ca79395e94a553d8f1a060a696ff5e1
J. Manuel Velasco <jmanuel.velasco@gmail.com>
2016-02-24 22:21:12 +0100

    Refactorizar código

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -33,21 +33,15 @@ class DefaultController extends Controller
     /**
      * @Route("/musas/{idMusa}", name="show-musas")
      */
-    public function showAction($idMusa)
+    public function showAction($idMusa, Request $request)
     {
         $repository = $this->getDoctrine()->getRepository('AppBundle:Musas');
         $publicationsMusas = $repository->find($idMusa);
-        $publications = $publicationsMusas->getIdPublication()->getValues();
 
-        if ($this->getUser()) {
-            $loggedUsername = $this->getUser()->getUsername();
-        } else {
-            $loggedUsername = '__off__';
-        }
-        //var_dump($publications);
         return $this->render('default/show.html.twig', array(
-            'publications' => $publications,
-            'loggedUsername' => $loggedUsername
+            'publications'  => $publicationsMusas->getIdPublication()->getValues(),
+            'musa'          => $publicationsMusas->getName(),
+            'returnPath' => $request->headers->get('referer')
         ));
 
     }
@@ -56,20 +50,14 @@ class DefaultController extends Controller
     /**
      * @Route("/publication/{idPublication}", name="show-publication")
      */
-    public function showPublicationAction($idPublication)
+    public function showPublicationAction($idPublication, Request $request)
     {
         $repository = $this->getDoctrine()->getRepository('AppBundle:Publications');
         $publication = $repository->findById($idPublication);
 
-        if ($this->getUser()) {
-            $loggedUsername = $this->getUser()->getUsername();
-        } else {
-            $loggedUsername = '__off__';
-        }
-        //var_dump($publications);
         return $this->render('default/showpublication.html.twig', array(
             'publications' => $publication,
-            'loggedUsername' => $loggedUsername
+            'returnPath' => $request->headers->get('referer')
         ));
 
     }
