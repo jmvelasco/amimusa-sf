5f8fff6b1707a21b058c6c23ee3b4f3bdc24abc8
manou <jmanuel.velasco@gmail.com>
2020-12-13 14:42:16 +0100

    Incluir Bot-Detect Captcha en el proyecto e integrarlo en el formulario de registro para protegerse de la creacion de cuentas por bots

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,7 +17,6 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
-            new AppBundle\AppUserBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new Captcha\Bundle\CaptchaBundle\CaptchaBundle()
         );
---
+++ src/AppBundle/Form/Type/RegistrationFormType.php
@@ -0,0 +1,28 @@
+<?php 
+// namespace AppBundle\Form\Type;
+
+// use Symfony\Component\Form\AbstractType;
+// use Symfony\Component\Form\FormBuilderInterface;
+
+// class RegistrationFormType extends AbstractType
+// {
+//     public function buildForm(FormBuilderInterface $builder, array $options)
+//     {
+//         parent::buildForm($builder, $options);
+
+//         $builder->add('captchaCode', 'captcha', array(
+//             'captchaConfig' => 'RegisterCaptcha',
+//             'label' => 'Retype the characters from the picture'
+//         ));
+//     }
+
+//     public function getParent()
+//     {
+//         return 'fos_user_registration';
+//     }
+
+//     public function getName()
+//     {
+//         return 'app_user_registration';
+//     }
+// }
